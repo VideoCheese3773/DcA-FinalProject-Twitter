b2a5508999385283c80b0277d7ba02b7395e7c2a
not gonna tell ya <eedduxx@gmail.com>
2023-03-23 18:01:01 -0500

    feat: improvements to left menu

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 //directorio
-export { default as Button } from './Button/Button.js'
 export { default as LeftMenu } from './leftmenu/leftmenu.js'
 export { default as MidMenu } from './midmenu/midmenu.js'
 export { default as RightMenu } from './rightmenu/rightmenu.js'
--- src/components/leftmenu/leftmenu.js
+++ src/components/leftmenu/leftmenu.js
@@ -10,11 +10,24 @@ class LeftMenu extends HTMLElement {
 
     render() {
         this.shadowRoot.innerHTML=`
-        <h1>Home</h1>
-        <h1>Explore</h1>
-        <h1>Configuration</h1>
+        <link rel="stylesheet" href="./index.css">
+        <section class="leftMenuSection">
+            <div>
+                <img src="./img/twitterLogo.png" class="menuIcon">
+                <h1>Home</h1>
+            </div>
+            <div>
+                <img src="./img/hashtag.png" class="menuIcon">
+                <h1>Explore</h1>
+            </div>
+            <div>
+                <img src="./img/cog.png" class="menuIcon">
+                <h1>Configuration</h1>
+            </div>
+        </section>
         `
     }
 }
 
 customElements.define('left-menu', LeftMenu)
+export default LeftMenu
