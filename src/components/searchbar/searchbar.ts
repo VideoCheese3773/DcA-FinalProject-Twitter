255d97b0d8e3d7e174fcf20a64a52dc4528fdc49
Sebastian Romero <nakosume@gmail.com>
2023-04-25 16:36:55 -0500

    searchbar converted from js to ts

--- src/components/searchbar/searchbar.ts
+++ src/components/searchbar/searchbar.ts
@@ -1,4 +1,4 @@
-class SearchBar extends HTMLElement{
+export default class SearchBar extends HTMLElement{
     constructor(){
         super();
         this.attachShadow({mode:'open'});
@@ -8,15 +8,18 @@ class SearchBar extends HTMLElement{
     }
 
     render(){
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML=""
+
         this.shadowRoot.innerHTML=`
         <link rel="stylesheet" href="./index.css">
-        <div class="searchCont">
+        <section class="searchCont">
         <input id="search" type="text" placeholder="Search...">
         <img src="./img/lupe.png" class="searchIcon">
         <img src="./img/cogAlt.png" class="searchIcon">
-        </div>
+        </section>
         `;
+        }
     }
 }
 customElements.define("search-bar",SearchBar)
-export default SearchBar
