b711b68d339bccf6bc1378de06d8976d7b260758
Sebastian Romero <nakosume@gmail.com>
2023-04-25 16:48:08 -0500

    refactor: added Post & SearchBar to export.ts

--- src/components/export.ts
+++ src/components/export.ts
@@ -2,3 +2,5 @@ export { default as Card } from "./card/card";
 export { default as Button} from "./Button/Button";
 
 export { default as LeftMenu } from "./leftmenu/leftmenu"
+export { default as Post} from "./post/post"
+export { default as SearchBar} from "./searchbar/searchbar"
