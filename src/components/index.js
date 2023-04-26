b4a500ebc7ec7d9aab1c81b0c4789ae6ad2dc4e6
not gonna tell ya <eedduxx@gmail.com>
2023-04-25 19:46:58 -0500

    feat: added left menu logged

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-//directorio
-export { default as LeftMenu } from './leftmenu/leftmenu.js'
-export { default as MidMenu } from './midmenu/midmenu.js'
-export { default as RightMenu } from './rightmenu/rightmenu.js'
-export { default as Post } from './post/post.js'
-export { default as SearchBar } from './searchbar/searchbar.js'
