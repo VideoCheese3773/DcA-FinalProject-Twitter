7f0cd559400781574dfe45cc0f74edd1bb4301f5
not gonna tell ya <eedduxx@gmail.com>
2023-04-25 19:00:37 -0500

    feat: log in wireframe

---
+++ src/components/LogIn/logIn.ts
@@ -0,0 +1,40 @@
+import styles from "./styles.css"
+
+class SignUp extends HTMLElement {
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" })
+    }
+
+    connectedCallback() {
+        if (this.shadowRoot) {
+            this.render()
+        }
+    }
+
+    render() {
+        this.shadowRoot!.innerHTML = ``
+
+        const css = this.ownerDocument.createElement("styles");
+        css.innerHTML = styles;
+        this.shadowRoot?.appendChild(css);
+
+        this.shadowRoot!.innerHTML += `
+        <link rel="stylesheet" href="./index.css">
+        <section>
+            <div>
+                <p>X</p>
+                <img src="./img/twitterLogo.png">
+                <h1>Log In On Twitter</h1>
+                <input type="text" placeholder="Username"></input>
+                <input type="password" placeholder="Password"></input>
+                <p>If you don't have an account, <a>Sign Up.</a></p>
+            </div>
+        </section>
+        `
+    }
+}
+
+customElements.define('sign-up', SignUp)
+export default SignUp
