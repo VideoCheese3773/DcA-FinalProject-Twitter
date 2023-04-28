13a80b8dcf6e73a40bf4e2ffae580532c8df0c12
VideoCheese3773 <eedduxx@gmail.com>
2023-04-28 18:33:01 -0500

    feat: finished usr config menu

--- src/components/user-config/UserConfig.ts
+++ src/components/user-config/UserConfig.ts
@@ -29,10 +29,12 @@ class UserConfig extends HTMLElement {
                 <img src="/img/user.png" class="menuIcon">
                 <h1 class="banner">User Configuration</h1>
                 <input class="input" type="text" placeholder="Username"></input>
+                <h1 class="banner">Biography</h1>
+                <input class="input" type="text" placeholder="User Bio"></input>
+                <h1 class="banner">New Password</h1>
                 <input class="input" type="password" placeholder="Password"></input>
                 <input class="input" type="password" placeholder="Confirm Password"></input>
-                <button class="button">Sign Up</button>
-                <p>If you already have an account, <a id="logInButton" class="logIn">log in.</a></p>
+                <button class="button">Save Changes</button>
             </div>
         </section>
         `
